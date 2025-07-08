ecb0262b2c9fb839d4f2c4ed944ec8516c66acee
MohammadHossan <hossan.dev@outlook.com>
2025-07-08 20:48:47 +0100

    Initial Commit

---
+++ SocialApp/SocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAppApp.swift
+//  SocialApp
+//
+//  Created by MohammadHossan on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
