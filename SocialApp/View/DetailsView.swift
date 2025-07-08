c386f54a8c7f36552a7e96eea942ea32b0ebf28c
MohammadHossan <hossan.dev@outlook.com>
2025-07-08 21:27:50 +0100

    Added the user details view

---
+++ SocialApp/View/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  SocialApp
+//
+//  Created by MohammadHossan on 08/07/2025.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
